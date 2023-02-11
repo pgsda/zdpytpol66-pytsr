e309d8e2c905765f71742c05748d1beb7702c4cd
Ewa Tryniszewska <eva.tryniszewska@gmail.com>
2023-02-11 10:42:26 +0100

    Własny wyjątek

--- wyjatki.py
+++ wyjatki.py
@@ -1,12 +1,16 @@
+class IncorrectDebt(Exception):
+    pass
+
+
 def zmien_saldo(saldo_poczatkowe, kwota, max_zadl):
     sk = saldo_poczatkowe + kwota
     if sk < -max_zadl:
-        raise ValueError(f'Kwota {sk} przekracza maksymalne zadłużenie {max_zadl}')
+        raise IncorrectDebt(f'Kwota {sk} przekracza maksymalne zadłużenie {max_zadl}')
     return sk
 
 
 try:
-    saldo_po_operacji = zmien_saldo(100, -150, 50)
+    saldo_po_operacji = zmien_saldo(100, -200, 50)
     print(saldo_po_operacji)
-except ValueError:
+except IncorrectDebt:
     print('Nie masz środków')
